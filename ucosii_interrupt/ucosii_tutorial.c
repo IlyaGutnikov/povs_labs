fc0484c60158b499ba7bc57c1753085dc02131d3
Ilya Gutnikov <IlyaGutnikov@lab1n00.iu3.bmstu.ru>
2015-10-27 15:07:14 +0400

    Начал делать индивидуальное задание.

--- ucosii_interrupt/ucosii_tutorial.c
+++ ucosii_interrupt/ucosii_tutorial.c
@@ -4,6 +4,9 @@
 #include "includes.h"
 #include "alt_ucosii_simple_error_check.h"
 #include "altera_avalon_pio_regs.h"
+#include "system.h"
+#include "alt_types.h"
+#include "sys/alt_irq.h"
 
 #include "system.h"
 
@@ -58,6 +61,66 @@ char sem_owner_task_name[20];
 /* Прототипы функций */
 int initOSDataStructs(void);
 int initCreateTasks(void);
+
+#define NONE_PRESSED 0xF
+#define DEBOUNCE 30000
+
+#define BTN_RIGHT_PRESSED 0x1
+#define BTN_LEFT_PRESSED 0x8
+#define BTN_CNTR_LEFT 0x4
+#define BTN_CNTR_RIGHT 0x2
+
+volatile int edge_capture;
+
+#ifdef ALT_ENHANCED_INTERRUPT_API_PRESENT
+    static void handle_button_interrupts(void* context)
+#else
+    static void handle_button_interrupts(void* context, alt_u32 id)
+#endif
+{
+    /* Cast context to edge_capture's type. It is important that this
+    be declared volatile to avoid unwanted compiler optimization. */
+    volatile int* edge_capture_ptr = (volatile int*) context;
+    /*
+    * Read the edge capture register on the button PIO.
+    * Store value.
+    */
+    *edge_capture_ptr =
+    IORD_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE);
+    /* Write to the edge capture register to reset it. */
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE, 0);
+    /* Read the PIO to delay ISR exit. This is done to prevent a
+    spurious interrupt in systems with high processor -> pio
+    latency and fast interrupts. */
+    IORD_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE);
+    printf("Handler captured!\n");
+}
+
+/* Initialize the button_pio. */
+static void init_button_pio()
+{
+    /* Recast the edge_capture pointer to match the
+    alt_irq_register() function prototype. */
+    void* edge_capture_ptr = (void*) &edge_capture;
+    /* Enable all 4 button interrupts. */
+    //можем обрабатывать со всех четырех линий.кнопок
+    IOWR_ALTERA_AVALON_PIO_IRQ_MASK(BUTTONS_BASE, 0xf);
+    /* Reset the edge capture register. */
+    // сбрасываем регистр edge capture
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE, 0x0);
+    /* Register the ISR. */
+    // задание обработчика прерываний
+    #ifdef ALT_ENHANCED_INTERRUPT_API_PRESENT
+        alt_ic_isr_register(BUTTONS_IRQ_INTERRUPT_CONTROLLER_ID,
+            BUTTONS_IRQ,
+            handle_button_interrupts,
+            edge_capture_ptr, 0x0);
+    #else
+        alt_irq_register( BUTTON_PIO_IRQ, edge_capture_ptr, handle_button_interrupts );
+    #endif
+
+    printf("Init done!\n");
+}
 
 void isr_button(void* context, alt_u32 id)
 {
@@ -217,6 +280,29 @@ void receive_task2(void* pdata)
   }
 }
 
+void get_sem_task2(void* pdata)
+{
+  INT8U return_code = OS_NO_ERR;
+  INT32U *msg;
+
+  while (1)
+  {
+
+	  int led = 0x01;
+
+	     edge_capture = 0xF;
+
+	     init_button_pio();
+
+	     printf("Simple\n");   // print a message to show that program is running
+
+	     IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LED_BASE,led); // write initial value
+
+  }
+}
+
+
+
 void  initialize_task(void* pdata)
 {
   INT8U return_code = OS_NO_ERR;
@@ -277,6 +363,9 @@ int initCreateTasks(void)
                              (void *)&send_task_stk[TASK_STACKSIZE-1],
                              SEND_TASK_PRIORITY);
   alt_ucosii_check_return_code(return_code);
+
+  //здесь создаем новую таску
+
 
   return 0;
 }
